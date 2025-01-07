f1e8d95569c281d5b274d533dca2cf3b0ba5048c
최은별 <99008137+dmsquf03@users.noreply.github.com>
2025-01-07 23:53:50 +0900

    [최은별] Feat: todo 리스트 추가 기능 구현 (#8)
    * feat: 리액트 프로젝트 생성성
    
    * feat: 전체 UI 구현 및 기능 구현 준비비
    
    * feat: todo 리스트 추가 기능 구현
    
    ---------
    
    Co-authored-by: ahnsui <139625508+ahnsui@users.noreply.github.com>

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,11 +1,39 @@
-const Editor = () => {
+import { useState, useRef } from "react";
+const Editor = ({ onCreate }) => {
+  const [content, setContent] = useState("");
+  const contentRef = useRef();
+
+  const onChangeContent = (e) => {
+    setContent(e.target.value);
+  };
+  console.log(content);
+
+  const onSubmit = () => {
+    if (content === "") {
+      contentRef.current.focus();
+    }
+    onCreate(content);
+  };
+
+  const onKeyDown = (e) => {
+    if (e.key == "Enter") {
+      onSubmit();
+    }
+  };
   return (
     <div className="flex justify-between h-24 pb-16 border-b text-sm">
       <input
+        onChange={onChangeContent}
+        value={content}
         className="w-9/12 border rounded p-2"
         placeholder="추가할 todo를 입력해주세요."
+        ref={contentRef}
+        onKeyDown={onKeyDown}
       />
-      <button className="border rounded-3xl px-4 bg-blue-400 text-white">
+      <button
+        onClick={onSubmit}
+        className="border rounded-3xl px-4 bg-blue-400 text-white"
+      >
         추가
       </button>
     </div>
--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,6 +1,8 @@
 import TodoItem from "./TodoItem";
 
-const List = () => {
+const List = ({ todos }) => {
+  console.log(todos);
+
   return (
     <div className="flex flex-col gap-8">
       <h1 className="text-2xl font-semibold">Todo List</h1>
@@ -8,9 +10,9 @@ const List = () => {
         className="w-full border rounded p-2 h-7 text-xs"
         placeholder="검색할 todo를 입력해주세요."
       />
-      <TodoItem />
-      <TodoItem />
-      <TodoItem />
+      {todos.map((todo) => {
+        return <TodoItem key={todo.id} todo={todo} />;
+      })}
     </div>
   );
 };
